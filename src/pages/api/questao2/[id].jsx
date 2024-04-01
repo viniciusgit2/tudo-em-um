156b437cecb364568eee84aafa7c00878a35954d
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-01 10:10:07 -0300

    com id

--- src/pages/api/questao2/[id].jsx
+++ src/pages/api/questao2/[id].jsx
@@ -3,6 +3,13 @@
 import Questoesx from "../bd/questoes/Questoesx"
 
 export default (req, res)=> {
-  
-  res.status(200).json(Questoesx[0].paraobjeto())
+  const idselecionado =+req.query.id
+ const Questselecionada= Questoesx.filter(Quest =>Quest.id ===idselecionado)
+ if (Questselecionada.length===1){
+ const questselecionada=Questselecionada[0]
+ res.status(200).json(questselecionada.paraobjeto())
+ }else{
+res.status(204).send()
+ }
+
 }
